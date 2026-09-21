b6e7409210a3a6198dab1057c19ff9649d219163
waroot32049-sys <waroot32049@gmail.com>
2026-09-21 15:51:43 +0700

    Add service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE="school-budget-v1";
+const ASSETS=["./","./index.html","./manifest.webmanifest"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
