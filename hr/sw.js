5aebb87f181ab9465b03e8ab35cf0c7898518080
waroot32049-sys <waroot32049@gmail.com>
2026-09-21 16:33:24 +0700

    Refresh HR app cache for attachment update

--- hr/sw.js
+++ hr/sw.js
@@ -1,4 +1,4 @@
-const CACHE='school-hr-v1';
+const CACHE='school-hr-v2';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
